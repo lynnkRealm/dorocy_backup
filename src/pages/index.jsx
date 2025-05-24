024a5abec87fa8c02f27ca92327d09914f96e285
lynnkrealm <akasarao3@gamil.com>
2025-05-24 21:14:57 +0900

    라우팅 고침

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,11 +31,9 @@ export default function Home() {
     // 토큰의 principal_type에 따른 허용 테이블 목록 전달
     const allowedTables = TABLES_BY_TYPE[principalType] || []
 
-    // 쿼리 또는 세션/컨텍스트로 넘기기. 간단히 쿼리로 넘긴 예시
-    router.replace({
-      pathname: '/dashboard',
-      query: { tables: allowedTables.join(',') },
-    })
+    // dashboard로 이동
+    router.replace('/dashboard')
+
   }, [router])
 
   return (
